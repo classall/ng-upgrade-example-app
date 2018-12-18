b857e715d26b29760bcaeb98763da216a72f1b70
classall <corentin.lassalle@outlook.fr>
2018-12-18 19:31:49 +0100

    - Add ng1 component

---
+++ src/ng1/components/ng1-component/ng1-component.js
@@ -0,0 +1,14 @@
+class Ng1Controller {
+
+  constructor() {
+    this.title = 'Ng1 Component !';
+  }
+
+}
+
+let Ng1Component = {
+  controller: Ng1Controller,
+  templateUrl: './ng1-component.html'
+};
+
+export default Ng1Component;
