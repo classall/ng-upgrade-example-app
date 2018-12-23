7328d0027b1d84f80e508844bb237cd528536846
classall <corentin.lassalle@outlook.fr>
2018-12-23 18:41:43 +0100

    - Update npm start command - Update readme

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -8,6 +8,7 @@ import browserify from 'browserify';
 import babelify from 'babelify';
 import ngAnnotate from 'browserify-ngannotate';
 import templateCache from 'gulp-angular-templatecache';
+import sequence from 'gulp-sequence';
 
 const interceptErrors = function(error) {
   var args = Array.prototype.slice.call(arguments);
@@ -22,7 +23,7 @@ const interceptErrors = function(error) {
   this.emit('end');
 };
 
-gulp.task('browserify', ['views'], () => {
+gulp.task('browserify', () => {
   return browserify('src/ng1/app.js')
     .transform(babelify, {
       presets: ['es2015']
@@ -46,4 +47,10 @@ gulp.task('views', function() {
     .pipe(gulp.dest('src/ng1'));
 });
 
-gulp.task('build', ['browserify']);
+gulp.task('watch', function() {
+  gulp.watch(['!src/ng1/dist/**/*.js', 'src/ng1/**/*.js'], ['browserify']);
+  gulp.watch('src/ng1/**/*.html', ['views']);
+});
+
+gulp.task('build', sequence('views', 'browserify'));
+gulp.task('default', sequence('views', 'browserify', 'watch'));
